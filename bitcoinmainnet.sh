f44e41e36a882fa8818590788264e08c32f15667
srihari kapu <sriharikapu@mail.com>
2019-03-13 10:48:19 +0800

    Create bitcoinmainnet.sh

---
+++ bitcoinmainnet.sh
@@ -0,0 +1,7 @@
+  sudo apt-add-repository ppa:bitcoin/bitcoin
+  sudo apt-get update
+  sudo apt-get install bitcoind
+  sudo apt-get install bitcoin-qt bitcoind
+  bitcoind -daemon
+  bitcoin-cli -getinfo
+  
